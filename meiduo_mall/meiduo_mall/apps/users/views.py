0a8b2e6f8d09427f203c359c5c3023e2a047e9e5
Ringo <ry.li@qq.com>
2021-12-08 22:30:57 +0800

    day59-4 用户注册之展示用户注册页面

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-# class Register():
-#     def get(self, requets):
+class RegisterView(View):
+    """用户注册"""
+
+    def get(self, requets):
+        """提供用户注册页面"""
+        return render(requets, 'register.html')
+
+class LoginView(View):
+    def get(self, requets):
+        return render(requets, 'login.html')
