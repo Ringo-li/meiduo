5deca642ac09b6366b821969f7e10bffeaea2605
Ringo-li <ry.li@qq.com>
2022-04-07 09:23:46 +0800

    添加版本号接口

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++ meiduo_mall/meiduo_mall/apps/contents/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('version/', views.VersionView.as_view(), name='version'),
 ]
--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django import http
 
 # Create your views here.
 class IndexView(View):
@@ -7,3 +8,13 @@ class IndexView(View):
     def get(self, request):
         return render(request, 'index.html')
 
+class VersionView(View):
+    # 返回版本号
+    def get(self, request):
+        """
+        :return: JSON
+        """
+        # 实现业务逻辑，使用username查询对应记录的条数。
+        version = "CI_COMMIT_TAG"
+        # 响应结果
+        return http.JsonResponse({'version': version})
